81fb35d422793f06721d49fb4c3b18f6c756ef19
GerardRome <gerardromee@gmail.com>
2021-04-23 16:55:58 -0500

    actualizacion en eliminar

--- JS/registroVenta.js
+++ JS/registroVenta.js
@@ -7,7 +7,7 @@ const productPriceUI = document.querySelector('#inputPriceUI');
 const productCantUI = document.querySelector('#inputCantUI');
 const inputsUI = document.querySelectorAll('input');
 const listaVentaProductosUI = document.querySelector('#list-products-sale');
-let productSaleList = localStorage.getItem('productos-venta') ? JSON.parse(localStorage.getItem('productos-venta')) : [];
+let productSaleList = [];
 let productsList = localStorage.getItem('productos') ? JSON.parse(localStorage.getItem('productos')) : [];
 let indexProduct = null;
 let productsFilter = '';
@@ -15,32 +15,37 @@ let productsFilter = '';
 // ============================ FUNCIONES =======================
 
 // ---------------- AGREGAR PRODUCTO PARA VENTA ------------------>
-const _agregarVenta = (producto, precio, cantidad) => {
+const _addSale = (producto, precio, cantidad) => {
    newSale = {
       producto: producto,
       precio: precio,
       cantidad: cantidad,
    };
    productSaleList.push(newSale);
+   _printSaleList();
+
+};
+
+const _deleteProduct = (index) =>{
+   productSaleList.splice(index,1)
+   _printSaleList();
+   console.log(productSaleList);
 };
 
 const _printSaleList = () =>{
    listaVentaProductosUI.innerHTML = ''
-   productSaleList.forEach(elem => {
+   productSaleList.forEach((elem, index) => {
       listaVentaProductosUI.innerHTML += `
          <li class="list-group-item d-flex justify-content-between align-items-start">
             <div class="ms-2 me-auto">
             <div class="fw-bold">${elem.producto}</div>
             Precio $ ${elem.precio}
             </div>
+            <i class="fas fa-trash my-1 mx-2" style="cursor: pointer;" onclick="_deleteProduct(${index})"></i>
             <span class="badge bg-primary rounded-pill">${elem.cantidad}</span>
          </li>
       `
    });
-};
-
-const _guardarVentaLS = ()=>{
-   localStorage.setItem('productos-venta', JSON.stringify(productSaleList));
 };
 
 const _statusChange = (input) =>{
@@ -48,19 +53,22 @@ const _statusChange = (input) =>{
 };
 
 const _validateVenta = () =>{
-   const status = null
+   let status = true
    if (searchTermUI.value === '') {
       _statusChange(searchTermUI);
-      status
+      status = false
    }if(productPriceUI.value === '') {
       _statusChange(productPriceUI);
-      status
+      status = false
    }if(productCantUI.value === '') {
       _statusChange(productCantUI);
-      status
-   }if(status === null) {
-      alert('completa todos los campos')
+      status = false
    }
+   if(status === false) {
+      alert('completa todos los campos')
+   }else{
+      _addSale(searchTermUI.value, productPriceUI.value, productCantUI.value)
+   };
 };
 
 // <------------------------------------------------------------------------------------
@@ -117,7 +125,6 @@ const _validateTextSearch = () =>{
 
 const _filterProducts = () =>{
    _validateTextSearch();
-   console.log(productsList,'que esta pasando -----------> D:');
    productsFilter = productsList.filter(prod => prod.nameProduct.includes(searchTermUI.value));
    _printProductsList(productsFilter);
 };
@@ -139,7 +146,6 @@ const _singOff = () =>{
 // ============================ EVENTOS ========================
 
 _validateUserActive();
-_printSaleList();
 
 searchTermUI.addEventListener('keyup', e =>{
    var keycode = e.keyCode;
@@ -158,11 +164,8 @@ inputsUI.forEach(input => input.addEventListener('focus', function() {
 
 formUI.addEventListener('submit', e => {
    e.preventDefault();
-   _agregarVenta(searchTermUI.value, productPriceUI.value, productCantUI.value);
    _validateVenta();
-   _printSaleList();
    formUI.reset();
-   _guardarVentaLS();
 });
 
 document.addEventListener('DOMContentLoaded', e => {
